e1766118394b53728d015bbeb5a3a2d8a7f89d86
Aigbogun Osayuwamen <99479545+levis17@users.noreply.github.com>
2023-01-04 00:18:21 +0100

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,4 +7,5 @@ def fizzbuzz():
             print("Fizz ", end="")
         elif number % 5 == 0:
             print("Buzz ", end="")
-        else: print("{} ".format(number), end="")
+        else:
+            print("{} ".format(number), end="")
