06851eb985782048458d2dcc0dc79a1c06798b08
Osayuwamen <aigbogunosayu@gmail.com>
2023-01-11 09:31:33 -0800

    uniq add

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3#
+#!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
     """Replace all occurrences of an element by another in a new list."""
@@ -7,4 +7,3 @@ def search_replace(my_list, search, replace):
         if new_list[i] == search:
             new_list[i] = replace
         return (new_list)
-
